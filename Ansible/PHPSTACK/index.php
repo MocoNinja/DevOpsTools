993ce2d48df9e69befe18e6d3f9ea956d056459a
MocoNinja <javiergonzaleztello@gmail.com>
2018-08-10 09:13:51 +0000

    Starting a simple php-dev-env
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

---
+++ Ansible/PHPSTACK/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>DevOps Traning</title>
+</head>
+
+<body>
+  <header><h1>La máquina ha sido provisionada con Ansible satisfactoriamente :)</h1></header>
+  <body id="php_test">
+    <?php
+      echo "<p>" . "Si php está correctamente instalado, esto debería visualizarse sin problemas" . "</p>";
+      for ($espacio = 0; $espacio < 5; $espacio++) {
+        echo "<br />";
+      }
+      phpinfo();
+    ?>
+  </body>
+</body>
+
